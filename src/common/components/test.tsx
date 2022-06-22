919912615a131aa637a01ac3be9b364f198d875e
Victor Gabriel <vgabriel.vg84@gmail.com>
2022-06-22 14:13:49 -0300

    feat: create src dir

---
+++ src/common/components/test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Test: React.FC = () => {
+  return (
+    <div>
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+export { Test };
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Test } from '#common/components';
+
+const App: React.FC = () => {
+  return <Test />;
+};
+
+export default App;
