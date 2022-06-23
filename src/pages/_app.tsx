2c1563c1549c7be1dab7bf6b698e9f36f8cbdbaa
Victor Gabriel <vgabriel.vg84@gmail.com>
2022-06-22 22:47:13 -0300

    feat: setup chackra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 
+import theme from '#common/theme';
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={theme.default}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
